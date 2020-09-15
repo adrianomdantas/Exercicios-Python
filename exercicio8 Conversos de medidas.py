6e57b1a6bdaf1a4052fb4c2770cb7da236874cde
Adriano Muniz <nanomd@gmail.com>
2020-09-15 17:14:15 -0300

    Exercicio 6 Conversor de medidas
    Exercicio em pyton

---
+++ exercicio8 Conversos de medidas.py
@@ -0,0 +1,6 @@
+metros = float(input('digite a metragem: '))
+centimetro = metros*100
+milimetros = metros*1000
+print('{} metros é igual a {} centimetros e {} milimetros'.format(metros, centimetro, milimetros))
+
+
