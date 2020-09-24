3a2192e25cc1c84b61e8d467f10c98d923edc4d2
Adriano Muniz <nanomd@gmail.com>
2020-09-24 16:09:24 -0300

    exercicio 13 guanabara

---
+++ exercicio10a.py
@@ -0,0 +1,2 @@
+real = float(input('quantos reais você tem? '))
+print('você consegue comprar ${:.2f} Dollars'.format(real*0.30581))
---
+++ exercicio13 Reajuste salarial.py
@@ -0,0 +1,2 @@
+salario = float(input('Qual seu salario atual?: '))
+print('seu saário com 15% de aumento será R${:.2f}'.format(salario*1.15))
