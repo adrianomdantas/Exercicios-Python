2b63f68432ca776db1c50bb6096f236070323afb
Adriano Muniz <nanomd@gmail.com>
2020-10-16 14:27:27 -0300

    Exercicio 20 corrigido

---
+++ exercicio20 Sorteando um aordem na lista.py
@@ -0,0 +1,12 @@
+import random
+a1 = 'adriano'
+a2 = 'jacqueline'
+a3 = 'rodrigo'
+a4 = 'ane'
+a5 = 'binho'
+a6 = 'kelly'
+a7 = 'allan'
+a8 = 'ciane'
+lista = [a1, a2, a3, a4, a5, a6, a7, a8]
+seq = random.sample(lista, 8)
+print('A ordem sorteada foi: ', seq)
