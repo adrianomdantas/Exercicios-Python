709653fc6f329e74abd55e090c6e581c5cbb2a89
Adriano Muniz <nanomd@gmail.com>
2021-03-16 00:25:49 -0300

    terceira parte do exercício

--- AnoBisextoNetacad.py
+++ AnoBisextoNetacad.py
@@ -1,6 +1,6 @@
 def is_year_leap(year):
 #
-# put your code here
+# Your code from LAB 4.3.1.6.
 #
     if year % 400 == 0:
         return True
@@ -9,14 +9,36 @@ def is_year_leap(year):
             return True
         else:
             return False
-            
-test_data = [1900, 2000, 2016, 1987]
-test_results = [False, True, True, False]
-for i in range(len(test_data)):
-	yr = test_data[i]
-	print(yr,"->",end="")
-	result = is_year_leap(yr)
-	if result == test_results[i]:
-		print("OK")
-	else:
-		print("Failed")
+
+
+def days_in_month(year, month):
+#
+# Your code from LAB 4.3.1.7.
+#
+    mes31 = [1,3,5,7,8,10,12]
+    mes30 = [4,6,9,11]
+    
+    if month == 2:
+        if is_year_leap(year) == True:
+            return 29
+        else:
+            return 28
+    else:
+        if month > 12 or month < 1:
+            return None
+        elif month in (mes31):
+            return 31
+        else:
+            return 30
+
+def day_of_year(year, month, day):
+#
+# Write your new code here.
+#
+    
+    if day > days_in_month(year, month) or day < 1:
+        return None
+    else:
+        return year, month, day
+
+print(day_of_year(2001, 4, 31))
