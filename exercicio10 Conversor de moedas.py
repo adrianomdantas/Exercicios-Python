e69be9d324e3644a9a4f2615c89b170aea4b55e0
Adriano Muniz <nanomd@gmail.com>
2021-07-24 11:49:00 -0300

    aepenas atualizando repositório

--- exercicio10 Conversor de moedas.py
+++ exercicio10 Conversor de moedas.py
@@ -1,2 +1,2 @@
 dollar = float(input('quantos dolares você quer comprar? '))
-print('você precisa de R${:.2f} reais'.format(dollar*3.27))
+print('você precisa de R${:.2f} reais'.format(dollar*5.00))
