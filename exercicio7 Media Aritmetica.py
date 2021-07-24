ef5025a186615258aec0cf35ed839fe49577d983
Adriano Muniz <nanomd@gmail.com>
2021-07-24 12:20:08 -0300

    Alterada a primeira letra para maiuscula

--- exercicio7 Media Aritmetica.py
+++ exercicio7 Media Aritmetica.py
@@ -2,7 +2,7 @@ n1 = float(input('digita a primeira nota: '))
 n2 = float(input('digite a segunda nota: '))
 media = (n1+n2)/2
 if media >= 7:
-    print('a media deste aluno é: {}, aprovado'.format(media))
+    print('A media deste aluno é: {}, aprovado'.format(media))
 else:
-    print('a média deste aluno é: {}, reprovado'.format(media))
+    print('A média deste aluno é: {}, reprovado'.format(media))
 
