95a118f7483fc092fc171d4f942e68ca18a117b1
Adriano Muniz <nanomd@gmail.com>
2021-07-24 12:15:55 -0300

    Alterada a primeira letra para maiuscula

--- exercicio6 Dobro triplo raiz.py
+++ exercicio6 Dobro triplo raiz.py
@@ -1,3 +1,3 @@
 n1 = int(input('digite um numero: '))
-print('você digitou {} \no dobro {} \nsua raiz {}'.format(n1, n1*2, n1**(1/2)))
+print('Você digitou {} \no dobro {} \nsua raiz {}'.format(n1, n1*2, n1**(1/2)))
 
