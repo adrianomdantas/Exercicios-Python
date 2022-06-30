a9b70707e875b69d08be044d22b5173991942cf3
Adriano Muniz <adriano.muniz@claro.com.br>
2022-06-29 23:03:45 -0300

    ajusta desafio 11

--- Desafios/desafio011.py
+++ Desafios/desafio011.py
@@ -1,4 +1,4 @@
 alt = float(input('Digite a altura da parede em metros: '))
 larg = float(input('Digite a largura da parede: '))
 area = alt * larg
-print(f'A área da parede é {area}m² e será necessário {area / 2:.1f} latas de timta para cobri-la')
+print(f'A área da parede é {area:.2f}m² e será necessário {area / 2:.1f} latas de timta para cobri-la')
