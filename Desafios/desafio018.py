80ee10f468903f3aa188bb27d75c1605ff62a8c2
Adriano Muniz <nanomd@gmail.com>
2022-07-01 23:31:36 -0300

    desafio 18

--- Desafios/desafio018.py
+++ Desafios/desafio018.py
@@ -1,3 +1,9 @@
-from math import sin, tan, cos , degrees
-ang = float(input('Digite um ângulo: '))
-print(sin(ang))
+from math import sin, tan, cos , degrees, radians
+num = float(input('Digite um ângulo: '))
+rad = radians(num)
+seno = sin(rad)
+cosseno = cos(rad)
+tangente =  tan(rad)
+print(f'O ângulo de {num} tem o seno de {seno:.2f}')
+print(f'O ângulo de {num} tem o cosseno de {cosseno:.2f}')
+print(f'O ângulo de {num} tem a tangente de {tangente:.2f}')
