3fcfce1bdbce02c7d5b84b1ecb0f76804898114b
Adriano Muniz <nanomd@gmail.com>
2022-07-13 11:25:09 -0300

    desfio 44

--- AulasGuanabara/Desafio044.md
+++ AulasGuanabara/Desafio044.md
@@ -5,7 +5,7 @@
  Elabore um programa que cacule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:  
 - À vista Dinheiro/cheque: 10% de desconto  
 - À vista no cartão: 5% de desconto  
-- Em até 2c no cartão: Preço normal  
+- Em até 2x no cartão: Preço normal  
 - 3x ou mais no cartão: 20% de juros  
  
 
---
+++ Desafios/desafio044.py
@@ -0,0 +1,17 @@
+preco = float(input('digite o valor do produto: '))
+print('Escolha a forma de pagamento')
+print('[ 1 ] À vista Dinheiro/cheque: 10% de desconto')
+print('[ 2 ] À vista no cartão: 5% de desconto')
+print('[ 3 ] Em até 2x no cartão: Preço normal')
+print('[ 4 ] 3x ou mais no cartão: 20% de juros')
+opcao = int(input('Digite a forma de pagamento: '))
+if opcao == 1:
+    print(f'O valor do prodto é R${preco:.2f}, com desconto de 10% fica R${preco * 0.9:.2f}')
+elif opcao == 2:
+    print(f'O valor do prodto é R${preco:.2f}, com desconto de 5% fica R${preco * 0.95:.2f}')
+elif opcao == 3:
+    print(f'O valor do prodto é R${preco:.2f}')
+elif opcao == 4:
+    print(f'O valor do prodto é R${preco:.2f}, com juros de 20% fica R${preco * 1.2:.2f}')
+else:
+    print('Opção inválida')
