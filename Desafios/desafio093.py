6cfd583368f2e7771b02afe82cfe3cd53d37bcdd
Adriano Muniz <nanomd@gmail.com>
2022-07-26 13:04:16 -0300

    desafio 93 py

---
+++ Desafios/desafio093.py
@@ -0,0 +1,18 @@
+jogador = dict()
+jogador['nome'] = str(input('Nome do Jogador: '))
+gols = list()
+partidas = int(input(f'Quantas partidas o {jogador["nome"]} jogou? '))
+for i in range(0, partidas):
+    gols.append(int(input(f'Quantos gols na partida {i}? ')))
+jogador['gols'] = gols
+print(40 * "=-")
+jogador['totgols'] = 0
+for g in gols:
+    jogador['totgols'] += g
+    
+#print(jogador) 
+print(40 * "=-")
+print(f'O Jogador {jogador["nome"]} jogou {partidas} partidas')
+print(jogador['gols'])
+for k, v in enumerate(jogador['gols']):
+    print(f'{"=>":>5} Na partida {k}, fez {v} gols.')
