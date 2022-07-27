074d09549eab1f8d54f4ad60ae26c6e33be7a537
Adriano Muniz <nanomd@gmail.com>
2022-07-27 12:59:20 -0300

    desafio 95 py atualiação

--- Desafios/desafio095.py
+++ Desafios/desafio095.py
@@ -3,15 +3,17 @@ gols = list()
 jogador = dict()
 
 while True:
-    jogador['nome'] = str(input('Nome: '))
+    print(40 * '-')
+    jogador['nome'] = str(input('Nome do Jogador: '))
     qtd_partidas = int(input(f'Quantas partidas o {jogador["nome"]} jogou? '))
     for i in range(0, qtd_partidas):
+        print('', end="   ")
         gols.append(int(input(f'Gols na partida {i}: ')))
     jogador['gols'] = gols.copy()
     gols.clear()
     jogadores.append(jogador.copy())
     opcao = str(input('Deseja cadastrar outro jogador? [S/N] ' )).strip().upper()
-    print(40 * '-')
+    
     while True:
         if opcao not in 'SN':
             opcao = str(input('!ERR!\nDeseja cadastrar outro jogador? [S/N] ' )).strip().upper()            
@@ -19,7 +21,61 @@ while True:
             break
     if opcao == 'N':
         break
-print(jogadores)
+#print(20 * '=-')
+#print(jogadores)
+#print(20 * '=-')
+
+for i in jogadores:
+    i['totgols'] = 0
+    for j in i['gols']:
+        i['totgols'] += j
+
+print(40 * '-')    
+print(f'{"cod":>4} {" Nome":<15} {"Gols":<15} {"Total":<25}')
+print(40 * '-')
+
+for p, i in enumerate(jogadores):
+    print(f'{p:>4} {i["nome"]:<15} {str(i["gols"]):<15} {i["totgols"]:<15}')
+#print(jogadores)
+
+while True:
+    print(40 * '-')
+    dadosj = int(input('Mostrar dados de qual jogador? 999 para sair '))
+    if dadosj >= len(jogadores) and dadosj != 999:
+        while True:
+            if dadosj >= len(jogadores) and dadosj != 999:
+                dadosj = int(input('!ERRO! não existe este codigo de jogador\nMostrar dados de qual jogador? 999 para sair '))
+            else:
+                break
+    else:
+        print(20 * '=-')
+        for p, d in enumerate(jogadores):
+            if p == dadosj:
+                print(f'Levantamento dos dados do jogador {d["nome"]}')
+                for t, u in enumerate(d['gols']):
+                    print('  ', end=' ')
+                    print(f'Na partida {t} fez {u} gols')
+    if dadosj == 999:
+        break
+print('<< FIM >>')
+    
+'''
+while True:
+    print(40 * '-')
+    dadosj = int(input('Mostrar dados de qual jogador? 999 para sair '))
+    if dadosj >= len(jogadores) and dadosj != 999:
+        dadosj = int(input('!ERRO! não existe este codigo de jogador\nMostrar dados de qual jogador? 999 para sair '))
+    else:
+        print(20 * '=-')
+        for p, d in enumerate(jogadores):
+            if p == dadosj:
+                print(f'Levantamento dos dados do jogador {d["nome"]}')
+                for t, u in enumerate(d['gols']):
+                    print('  ', end=' ')
+                    print(f'Na partida {t} fez {u} gols')
+    if dadosj == 999:
+        break
+print('<< FIM >>')'''
 
 
 
